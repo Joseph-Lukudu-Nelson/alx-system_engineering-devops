e05598da00747834ab2104feced9897ae07c10b7
Joseph-Lukudu-Nelson <jkudus2022@gmail.com>
2023-03-30 07:04:57 -0700

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#create a Puppet, create a file in /tmp.
+file{'/tmp/school':
+	ensure => file,
+	       content => 'I love Puppet',
+	       owner => 'www-data',
+	       group => 'www-data',
+	       mode => '0744',
+
+}
